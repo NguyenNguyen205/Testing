a211e96a4b36ec8f08b3097084aceedf753ba0ed
NguyenNguyen205 <92852929+NguyenNguyen205@users.noreply.github.com>
2023-02-24 09:38:48 +0700

    Third test

--- fashiondiffusion/main.py
+++ fashiondiffusion/main.py
@@ -6,6 +6,7 @@ from diffusers import EulerAncestralDiscreteScheduler
 import sys, datetime
 from flask import Flask
 from uuid import uuid4
+import time
 
 # set up firebase credentials, put the firebase admin sdk in the same directory as this python file
 cred = credentials.Certificate("/greenieverse-firebase-adminsdk-e3b5a-f38879b828.json")
@@ -13,10 +14,12 @@ firebase_admin.initialize_app(cred, {
     'storageBucket': 'greenieverse.appspot.com'
 })
 
+start = time.time()
 # Install pre-trained pipeline from hugging face
 pipe = DiffusionPipeline.from_pretrained("22h/vintedois-diffusion-v0-1")
 # pipe = pipeline.to("cuda") # Use cuda only if the GPU is nvidia
-
+end = time.time()
+print("Time loading: ", end - start)
 # Generate image 
 
 
@@ -26,14 +29,18 @@ height = 512
 width = 512
 cfg_scale = 10 # guidance_scale
 seed = torch.Generator("cuda").manual_seed(44) # generator
-step = 50 # num_inference_steps
+step = 25 # num_inference_steps
 negative = "out of frame, duplicate, watermark, signature, text, ugly, morbid, mutated, deformed, blurry, bad anatomy, bad proportions, cloned face, disfigured, fused fingers, fused limbs, too many fingers, long neck, twisted face, three legs"
 # prompt = sys.argv[1]
 prompt = "the storm buried in the light"
 
 # image generation
+start = time.time()
 image = pipe(prompt, num_inference_steps=step, guidance_scale=cfg_scale, generator=seed, negative_prompt=negative).images[0]
 image.save(f"test.png")
+ 
+end = time.time()
+print("Generating time: ", end-start)
 
 
 
